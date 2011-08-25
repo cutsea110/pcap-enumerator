9ab9ccf050a74f2b91168ba50f9a6e3c786329ee
Katsutoshi Itoh <cutsea110@gmail.com>
2011-08-25 21:50:23 +0900

    arrange types.

--- Network/Pcap/Enumerator.hs
+++ Network/Pcap/Enumerator.hs
@@ -3,28 +3,28 @@ module Network.Pcap.Enumerator
        , enumLive
        ) where
 
+import Control.Monad.State
 import Data.ByteString (ByteString)
 import Data.ByteString.Char8 ()
 import Data.Enumerator hiding (map, filter)
 import Data.Int (Int64)
 import Network.Pcap
 
-enumOffline :: FilePath -> Enumerator (PktHdr, ByteString) IO b
+enumOffline :: (MonadIO m) => FilePath -> Enumerator (PktHdr, ByteString) m b
 enumOffline path step = do
   h <- tryIO $ openOffline path
   let iter = enumPcap1 h step
   Iteratee $ runIteratee iter
 
-enumLive :: String -> Int -> Bool -> Int64 -> Enumerator (PktHdr, ByteString) IO b
+enumLive :: (MonadIO m) => String -> Int -> Bool -> Int64 -> Enumerator (PktHdr, ByteString) m b
 enumLive name snaplen promisc timeout step = do
   h <- tryIO $ openLive name snaplen promisc timeout
   let iter = enumPcap1 h step
   Iteratee $ runIteratee iter
 
-enumPcap1 :: PcapHandle -> Enumerator (PktHdr, ByteString) IO b
+enumPcap1 :: (MonadIO m) => PcapHandle -> Enumerator (PktHdr, ByteString) m b
 enumPcap1 h = checkContinue0 $ \lp k -> do
   pkt@(hdr, _) <- tryIO $ nextBS h
   if (hdrCaptureLength hdr == 0)
     then continue k
     else k (Chunks [pkt]) >>== lp
-
